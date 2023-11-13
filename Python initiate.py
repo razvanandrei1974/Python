e795185f8d5839811e65126f52189720077cf663
Ungar Razvan <144438182+razvanandrei1974@users.noreply.github.com>
2023-11-13 16:44:17 +0200

    Add files via upload

---
+++ Python initiate.py
@@ -0,0 +1,63 @@
+print("Ford Mondeo!")
+
+if 5 > 2:
+ print("Five is greater than two!")
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(x)
+
+# output variabiles
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = 5
+y = 10
+print(x + y)
+print(x * y)
+
+# Python iterators
+
+mytuple = ("Ford", "Mondeo", "Hatcback")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+# Python lists
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+
+# Global Variables
+for x in "Razvan":
+  print(x)
+
+#Python Booleans
+# Varianta 1
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+# Varianta 2
+
+a = 20
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+
