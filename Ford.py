1d8b35d206b13c10a102e09604a695faacf9fe27
Ungar Razvan <144438182+razvanandrei1974@users.noreply.github.com>
2023-11-13 10:19:16 +0200

    Add files via upload

---
+++ Ford.py
@@ -0,0 +1,35 @@
+def print_hi(Ford_Mondeo):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Am achizitionat un {Ford_Mondeo}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Ford_Mondeo')
+
+
+def print_hi(Ford_Mondeo):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Are kilometrajul {145000}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Ford_Mondeo')
+
+
+def print_hi(Ford_Mondeo):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Are culoarea {"Argintiu"}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Ford_Mondeo')
+
+
+    def print_hi(Ford_Mondeo):
+        # Use a breakpoint in the code line below to debug your script.
+        print(f'Este {"inmatriculat"}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+    # Press the green button in the gutter to run the script.
+    if __name__ == '__main__':
+        print_hi('Ford_Mondeo')
