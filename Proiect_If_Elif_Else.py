fe280251c064512c00afd7599ea0da20eea6f513
Ungar Razvan <144438182+razvanandrei1974@users.noreply.github.com>
2023-11-23 16:44:31 +0200

    Add files via upload

--- Proiect_If_Elif_Else.py
+++ Proiect_If_Elif_Else.py
@@ -5,18 +5,15 @@
 # De asemenea, atât pentru seniori, cât și pentru non seniori se va aplica o taxă de lux de 3% dacă vor călători în clasa I (în orice sezon) sau 1% taxă de gestiune în caz contrar.
 ###
 
-age = 72
+age = 67
 
 if ((age >= 65)):
     print("Celor cu varsta de minim 65 de ani li se va oferi o reducere de 15% ")
 else:
     print("Nu se acorda reducere")
 decizie_discount = "Celor cu varsta de minim 65 de ani li se va oferi o reducere de 15% "
-VALOARE_DISCOUNT = print(decizie_discount[64:68])
-
-discount = 15
-Discount_oferit = discount
-print("Discount-ul oferit este de {:.2f}%".format(Discount_oferit))
+var = (decizie_discount[64:68])
+print("Reducerea oferita este de " + (var))
 
 age < 65
 insotit = "De un copil"
@@ -27,26 +24,23 @@ elif insotit == "De un copil" :
 else:
     print("Nu se acorda reducere ")
 decizie_discount = "Atat seniorilor cat si non-seniorilor li se va oferi o reducere de 10% daca sunt insotiti de un copil"
-print(decizie_discount[67:70])
+var = (decizie_discount[67:70])
+print("Discountul oferit este de " + (var))
 
-discount = 10
-Discount_oferit = discount
-print("Discount-ul oferit este de {:.2f}%".format(Discount_oferit))
+
 #
 age < 65
 anotimp = "iarna"
 if age < 65 :
     print("Nu se va acorda reducerea de 10%")
-elif anotimp == "toamna" :
+elif anotimp == "iarna" :
     print("Atat seniorilor cat si non-seniorilor li se va oferi o reducere de 10% daca calatoresc iarna")
 else:
-    print("Nu se acorda reducere ")
+    print("Nu se acorda reducerea de 10% ")
 decizie_discount = "Atat seniorilor cat si non-seniorilor li se va oferi o reducere de 10% daca calatoresc iarna"
-print(decizie_discount[67:70])
-Discount = decizie_discount[67:70]
-discount = 10
-Discount_oferit = discount
-print("Discount-ul oferit este de {:.2f}%".format(Discount_oferit))
+var = (decizie_discount[67:70])
+print("Discountul oferit este de " + (var))
+
 
 age < 65
 clasa_tren  = "Clasa I"
@@ -57,7 +51,6 @@ elif clasa_tren == "Clasa I" :
     print("Atat seniorilor cat si non-seniorilor li se va calcula o taxa de lux de 3% daca circula la Clasa I")
 else:
     print("Nu se calculeaza taxa de lux ")
-
-taxa_de_lux = 3
-Taxa_de_incasat = taxa_de_lux
-print("Taxa de lux pe care trebuie sa o plateasca este de {:.2f}%".format(Taxa_de_incasat))
+decizie_taxa = "Atat seniorilor cat si non-seniorilor li se va calcula o taxa de lux de 3 % daca circula la Clasa I"
+var = (decizie_taxa[72:75])
+print("Taxa de lux care trebuie platita este de " + (var))
